e505f4cab03c2b3f7419cb6294b28caf4f96723a
ReedKrawiec <reedkrawiec@gmail.com>
2021-06-16 16:04:40 -0400

    Basic Testing

--- main.py
+++ main.py
@@ -1,86 +1,99 @@
 #!/usr/bin/env python
 
-from lib.fenparser import FenParser 
+from lib.fenparser import FenParser
 from PIL import Image
 from PIL import ImageDraw
 from operator import itemgetter
 import os
-
+from PIL import ImageChops
 # http://wordaligned.org/articles/drawing-chessboards
 # https://github.com/tlehman/fenparser
 
-def paintCheckerBoard(board,darkColor):
-	height, width = board.size
-	draw = ImageDraw.Draw(board)
-	if height != width:
-		raise Exception("Height unequal to width")
-	for y in range(0,8):	
-		for x in range(0,8,2):
-			#Four pairs of dark then light must be painted per row
-			squareSize = width/8
-			firstIsColored = y % 2 == 0
-			startSquareOffset = 1 if firstIsColored else 0
-			start = ((x + startSquareOffset) * squareSize, y * squareSize)
-			end = ((x + startSquareOffset) * squareSize + squareSize - 1, y * squareSize + squareSize - 1)
-			draw.rectangle([start,end],darkColor)
-	return board;
+
+def paintCheckerBoard(board, darkColor):
+    height, width = board.size
+    draw = ImageDraw.Draw(board)
+    if height != width:
+        raise Exception("Height unequal to width")
+    for y in range(0, 8):
+        for x in range(0, 8, 2):
+            # Four pairs of dark then light must be painted per row
+            squareSize = width/8
+            firstIsColored = y % 2 == 0
+            startSquareOffset = 1 if firstIsColored else 0
+            start = ((x + startSquareOffset) * squareSize, y * squareSize)
+            end = ((x + startSquareOffset) * squareSize +
+                   squareSize - 1, y * squareSize + squareSize - 1)
+            draw.rectangle([start, end], darkColor)
+    return board
+
 
 def loadPiecesFolder(path):
-	whitePath = os.path.join(path,"white")
-	blackPath = os.path.join(path,"black")
-	wPath = lambda piece: os.path.join(whitePath,piece + ".png")
-	bPath = lambda piece: os.path.join(blackPath,piece + ".png")
-	pieceImages = {
-		"p": Image.open(bPath("Pawn")).convert("RGBA"),
-		"P": Image.open(wPath("Pawn")).convert("RGBA"),
-		"r": Image.open(bPath("Rook")).convert("RGBA"),
-		"R": Image.open(wPath("Rook")).convert("RGBA"),
-		"n": Image.open(bPath("Knight")).convert("RGBA"),
-		"N": Image.open(wPath("Knight")).convert("RGBA"),
-		"b": Image.open(bPath("Bishop")).convert("RGBA"),
-		"B": Image.open(wPath("Bishop")).convert("RGBA"),
-		"q": Image.open(bPath("Queen")).convert("RGBA"),
-		"Q": Image.open(wPath("Queen")).convert("RGBA"),
-		"k": Image.open(bPath("King")).convert("RGBA"),
-		"K": Image.open(wPath("King")).convert("RGBA")
-		}
-	def load(board):
-		pieceSize = int(board.size[0]/8)
-		for piece in pieceImages:
-			pieceImages[piece] = pieceImages[piece].resize((pieceSize,pieceSize))
-		return pieceImages
-	return load
+    whitePath = os.path.join(path, "white")
+    blackPath = os.path.join(path, "black")
+    def wPath(piece): return os.path.join(whitePath, piece + ".png")
+    def bPath(piece): return os.path.join(blackPath, piece + ".png")
+    pieceImages = {
+        "p": Image.open(bPath("Pawn")).convert("RGBA"),
+        "P": Image.open(wPath("Pawn")).convert("RGBA"),
+        "r": Image.open(bPath("Rook")).convert("RGBA"),
+        "R": Image.open(wPath("Rook")).convert("RGBA"),
+        "n": Image.open(bPath("Knight")).convert("RGBA"),
+        "N": Image.open(wPath("Knight")).convert("RGBA"),
+        "b": Image.open(bPath("Bishop")).convert("RGBA"),
+        "B": Image.open(wPath("Bishop")).convert("RGBA"),
+        "q": Image.open(bPath("Queen")).convert("RGBA"),
+        "Q": Image.open(wPath("Queen")).convert("RGBA"),
+        "k": Image.open(bPath("King")).convert("RGBA"),
+        "K": Image.open(wPath("King")).convert("RGBA")
+    }
 
-def paintPiece(board,cord,image):
-	height, width = board.size
-	pieceSize = int(width/8)
-	x = cord[0]
-	y = cord[1]
-	position = lambda val: int(val * pieceSize)
-	box = (position(x),position(y),position(x + 1),position(y + 1))
-	_,_,_,alpha = image.split()
-	Image.Image.paste(board,image,box,alpha)
-	return board
-def paintAllPieces(board,parsed,pieceImages):	
-	for y in range(0,len(parsed)):
-		for x in range(0,len(parsed[y])):
-			piece = parsed[y][x]
-			if piece != " ":
-				board = paintPiece(board,(x,y),pieceImages[piece])
-	return board
+    def load(board):
+        pieceSize = int(board.size[0]/8)
+        for piece in pieceImages:
+            pieceImages[piece] = pieceImages[piece].resize(
+                (pieceSize, pieceSize))
+        return pieceImages
+    return load
 
-def fenToBoardImage(fen, squarelength, pieceSet, darkColor, lightColor):
-	board = Image.new("RGB",(squarelength * 8,squarelength * 8),lightColor)
-	parsedBoard = FenParser(fen).parse()
-	board = paintCheckerBoard(board,darkColor)
-	board = paintAllPieces(board,parsedBoard,pieceSet(board))
-	return board
 
-fenToBoardImage(
-	fen = "8/5N2/4p2p/5p1k/1p4rP/1P2Q1P1/P4P1K/5q2 w - - 15 44",
-	squarelength =  125,
-	pieceSet = loadPiecesFolder("C:\\Users\\ReedK_000\\Documents\\GitHub\\fen-to-board-image\\pieces"),
-	darkColor = "#79a65d",
-	lightColor = "#daf2cb"
-).show()
+def paintPiece(board, cord, image):
+    height, width = board.size
+    pieceSize = int(width/8)
+    x = cord[0]
+    y = cord[1]
+    def position(val): return int(val * pieceSize)
+    box = (position(x), position(y), position(x + 1), position(y + 1))
+    _, _, _, alpha = image.split()
+    Image.Image.paste(board, image, box, alpha)
+    return board
 
+
+def paintAllPieces(board, parsed, pieceImages):
+    for y in range(0, len(parsed)):
+        for x in range(0, len(parsed[y])):
+            piece = parsed[y][x]
+            if piece != " ":
+                board = paintPiece(board, (x, y), pieceImages[piece])
+    return board
+
+
+def fenToBoardImage(fen, squarelength, pieceSet, darkColor, lightColor, flipped=False):
+    board = Image.new("RGB", (squarelength * 8, squarelength * 8), lightColor)
+    parsedBoard = FenParser(fen).parse()
+    board = paintCheckerBoard(board, darkColor)
+    if flipped:
+        parsedBoard.reverse()
+    board = paintAllPieces(board, parsedBoard, pieceSet(board))
+    return board
+
+
+if __name__ == '__main__':
+    fenToBoardImage(
+        fen="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+        squarelength=100,
+        pieceSet=loadPiecesFolder("./test/pieces2"),
+        darkColor="#909090",
+        lightColor="#fffefe",
+        flipped=True
+    ).save("./test/boards/board5.png")
---
+++ test/test.py
@@ -0,0 +1,71 @@
+import unittest
+import os
+os.sys.path.append(os.path.abspath(".."))
+from main import fenToBoardImage, loadPiecesFolder
+from PIL import Image
+from PIL import ImageDraw
+from operator import itemgetter
+from PIL import ImageChops
+
+
+class FenToBoardImageTest(unittest.TestCase):
+    def test_outputs(self):
+        image1 = fenToBoardImage(
+            fen="8/5N2/4p2p/5p1k/1p4rP/1P2Q1P1/P4P1K/5q2 w - - 15 44",
+            squarelength=125,
+            pieceSet=loadPiecesFolder(
+                "./pieces"),
+            darkColor="#79a65d",
+            lightColor="#daf2cb"
+        )
+        image2 = Image.open("./boards/board1.png")
+        diff = ImageChops.difference(image1, image2)
+        self.assertEqual(diff.getbbox(),None)
+        
+    def test_board_colors(self):
+        image1 = fenToBoardImage(
+            fen="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+            squarelength=100,
+            pieceSet=loadPiecesFolder("./pieces"),
+            darkColor="#D18B47",
+            lightColor="#FFCE9E"
+        )
+        image2 = Image.open("./boards/board2.png")
+        diff = ImageChops.difference(image1, image2)
+        self.assertEqual(diff.getbbox(),None)
+
+    def test_pieces(self):
+        image1 = fenToBoardImage(
+            fen="rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2 ",
+            squarelength=100,
+            pieceSet=loadPiecesFolder("./pieces2"),
+            darkColor="#909090",
+            lightColor="#fffefe"
+	    )
+        image2 = Image.open("./boards/board3.png")
+        diff = ImageChops.difference(image1, image2)
+        self.assertEqual(diff.getbbox(),None)
+    
+    def test_flip(self):
+        original = fenToBoardImage(
+            fen="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+            squarelength=100,
+            pieceSet=loadPiecesFolder("./pieces2"),
+            darkColor="#909090",
+            lightColor="#fffefe"
+        )
+        flipped = fenToBoardImage(
+            fen="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+            squarelength=100,
+            pieceSet=loadPiecesFolder("./pieces2"),
+            darkColor="#909090",
+            lightColor="#fffefe",
+            flipped=True
+        )
+        original_image = Image.open("./boards/board4.png")
+        flipped_image = Image.open("./boards/board5.png")
+        self.assertEqual(ImageChops.difference(original,original_image).getbbox(),None)
+        self.assertEqual(ImageChops.difference(flipped,flipped_image).getbbox(),None)
+
+if __name__ == '__main__':
+    unittest.main()
