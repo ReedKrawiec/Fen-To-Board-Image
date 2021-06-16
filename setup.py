68aab8c9b1a2d28cd1d8e762182ce275a6908d56
ReedKrawiec <reedkrawiec@gmail.com>
2021-06-16 17:32:08 -0400

    Package init

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+     name='fenToBoardImage',  
+     version='0.1',
+     scripts=['fenToBoardImage.py'] ,
+     author="Reed Krawiec",
+     author_email="reedkrawiec@gmail.com",
+     description="Create PIL images from chess Fen strings",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     keywords=["chess fen Pillow board"],
+     license="MIT",
+     install_requires=["Pillow"],
+     url="https://github.com/ReedKrawiec/fenToBoardImage",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+         "Topic :: Scientific/Engineering :: Image Processing"
+     ],
+ )
